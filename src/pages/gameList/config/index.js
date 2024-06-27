bf1d40eb15dfb2300e14fa97539959bb8da69af7
olivertate9 <kolomytsev95@gmail.com>
2024-06-27 18:29:59 +0300

    changed dns

--- src/pages/gameList/config/index.js
+++ src/pages/gameList/config/index.js
@@ -1,6 +1,6 @@
 const config = {
     // Services
-    GAMES_SERVICE: 'http://videogames-load-balancer-1719973211.eu-north-1.elb.amazonaws.com',
+    GAMES_SERVICE: 'https://videogames-load-balancer-1719973211.eu-north-1.elb.amazonaws.com',
     UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
 };
 

9f1c271fc89eb5a5113c313f190de348f68b9eea
olivertate9 <kolomytsev95@gmail.com>
2024-06-27 17:41:15 +0300

    changed dns

--- src/pages/gameDetails/config/index.js
+++ src/pages/gameDetails/config/index.js
@@ -1,5 +1,5 @@
 const config = {
-    GAMES_SERVICE: 'https://ec2-13-53-173-115.eu-north-1.compute.amazonaws.com:1000',
+    GAMES_SERVICE: 'https://ec2-13-53-173-115.eu-north-1.compute.amazonaws.com:443',
     UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
 };
 
--- src/pages/gameList/config/index.js
+++ src/pages/gameList/config/index.js
@@ -1,6 +1,6 @@
 const config = {
     // Services
-    GAMES_SERVICE: 'https://ec2-13-53-173-115.eu-north-1.compute.amazonaws.com:1000',
+    GAMES_SERVICE: 'https://ec2-13-53-173-115.eu-north-1.compute.amazonaws.com:443',
     UI_URL_PREFIX: process.env.REACT_APP_UI_URL_PREFIX || '',
 };
 
